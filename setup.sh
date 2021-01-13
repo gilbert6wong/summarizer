c3c5287268479945273cce2d9b04657bc1904c75
Gilbert Wong <gilbert6wong@gmail.com>
2021-01-12 19:58:01 -0500

    fixed git issue

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 mkdir -p ~/.streamlit
-=======
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"gilbert6wong@gmail.com\"
-" > ~/.streamlit/credentials.toml
->>>>>>> 5a0f5da0d9a6626c1403fae639a0dbee595d81ae
 echo "[server]
 headless = true
 port = $PORT
